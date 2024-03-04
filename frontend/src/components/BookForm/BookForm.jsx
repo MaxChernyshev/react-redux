424a8c05087d0b6e240990713dccdfd6da513359
MaxChernyshev <maksim.chernyshev@gmail.com>
2024-03-03 19:52:31 -0800

    components added

---
+++ frontend/src/components/BookForm/BookForm.jsx
@@ -0,0 +1,11 @@
+const BookForm = () => {
+    return (
+        <div className="app-block book-form">
+            <h2>
+                Add new book
+            </h2>
+        </div>
+    )
+}
+
+export default BookForm
